5989cd9a8188b47364e38cbcd53a16e5734c7242
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2021-12-27 20:29:05 +0300

    Code update

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -13,3 +13,42 @@ void PrintArray(int[] col)
     }  
     Console.WriteLine();
 }
+
+int[] FillArray(int[] arr)
+{
+    for(int j=0; j<arr.Length; j++)   // Заполнение массива случайными числами
+        {
+            arr[j] = new Random().Next(minValue,maxValue);
+        } 
+    return arr;
+}
+
+int[] SortArray(int[] arr)
+{
+    int n = 0;
+    int m = 0;
+    for(int i=0; i<arr.Length;i++)
+    {
+        if(arr[i]%2==0) 
+        {
+            n++;
+        }
+    }
+    int[] arrayB = new int[n];
+    for(int i=0; i<arr.Length;i++)
+    {
+        if(arr[i]%2==0) 
+        {
+            arrayB[m] = arr[i];
+            m++;
+        }
+    }
+    
+    return arrayB;
+}
+
+arrayA = FillArray(arrayA);
+Console.Write("First array ");
+PrintArray(arrayA);
+Console.Write("Second array ");
+PrintArray(SortArray(arrayA));
