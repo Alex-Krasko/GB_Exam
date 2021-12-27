35046db434de4d682b28aee3597481150d3d85bc
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2021-12-27 20:27:13 +0300

    Code added

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿int [] arrayA = new int[50];
+int minValue = 0;
+int maxValue = 100;
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+            Console.Write(col[position] +" ");
+            position++;
+    }  
+    Console.WriteLine();
+}
