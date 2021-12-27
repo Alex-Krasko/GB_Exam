29b787ad517102c04ae30cd4058f5960e2dac2c3
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2021-12-27 20:58:59 +0300

    task2 created

---
+++ Task2/Program.cs
@@ -0,0 +1,52 @@
+﻿int [] arrayA = new int[50];
+int minValue = 0;
+int maxValue = 100;
+
+int[] FillArray(int[] arr)   // Заполнение массива случайными числами
+{
+    for(int j=0; j<arr.Length; j++) 
+        {
+            arr[j] = new Random().Next(minValue,maxValue);
+        } 
+    return arr;
+}
+
+void PrintArray(int[] col)  // Вывод массива в консоль
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+            Console.Write(col[position] +" ");
+            position++;
+    }  
+    Console.WriteLine();
+}
+
+int[] SortArray(int[] arr)  // Поиск чисел больше 8 в массиве А и помещение их в массив Б
+{
+    int n = 0;
+    int m = 0;
+    for(int i=0; i<arr.Length;i++)
+    {
+        if(arr[i] > 8) 
+        {
+            n++;
+        }
+    }
+    int[] arrayB = new int[n];
+    for(int i=0; i<arr.Length;i++)
+    {
+        if(arr[i] > 8) 
+        {
+            arrayB[m] = arr[i];
+            m++;
+        }
+    }
+    return arrayB;
+}
+
+arrayA = FillArray(arrayA);
+Console.Write("First array: ");
+PrintArray(arrayA);
+Console.Write("Second array: ");
